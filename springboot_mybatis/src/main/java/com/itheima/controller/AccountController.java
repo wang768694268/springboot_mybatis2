bc4b1ab4592ab33a18619da38ec71b6a8243cdc7
jinchengwang <768694268@qq.com>
2018-11-14 16:45:09 +0800

    1 Signed-off-by: jinchengwang <768694268@qq.com> 修改1

---
+++ springboot_mybatis/src/main/java/com/itheima/controller/AccountController.java
@@ -0,0 +1,37 @@
+package com.itheima.controller;
+
+import com.itheima.domain.Account;
+import com.itheima.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AccountController {
+    @Autowired
+    private AccountService accountService;
+
+    /**
+     * 查询所有
+     * @return
+     */
+    @RequestMapping("findAll")
+    public List<Account> findAll() {
+        List<Account> accountList = accountService.findAll();
+        return accountList;
+    }
+
+    /**
+     * 添加用户
+     */
+    @RequestMapping("saveAccount")
+    public void saveAccount() {
+        Account account = new Account();
+        account.setName("wang");
+        account.setMoney(111.1);
+        accountService.saveAccount(account);
+
+    }
+}
